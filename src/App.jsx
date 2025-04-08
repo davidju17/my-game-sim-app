bec80335460bc9366b6e90b30e8eb947dfd1c707
David Monroy <davidju17@gmail.com>
2025-04-08 16:49:44 +1000

    add players headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import React, { useState } from 'react';
+
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+}
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-
-      </div>  
+    <main>
+      <div id='game-container'>
+          <ol id='players'>
+          <li>
+            {/* for styling reasons use span */}
+            <span className='player-name'>{PLAYERS.X}</span>
+            <span className='player-symbol'>X</span>
+          </li>
+          <li>
+            <span className='player-name'>{PLAYERS.O}</span>
+            <span className='player-symbol'>O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
     </>
   )
 }
