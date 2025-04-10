b34b5577d839fb34d7a694183dc6d4508b5945e9
David Monroy <davidju17@gmail.com>
2025-04-10 12:11:04 +1000

    handle player name changed

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
 
+  const [players, setPlayers] = useState({ PLAYERS })
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
 
@@ -39,24 +40,22 @@ function App() {
 
   // derive state from gameTurns 
   for (const turn of gameTurns) {
-      const { square, player } = turn;
-      const { row, col } = square;
+    const { square, player } = turn;
+    const { row, col } = square;
 
-      gameBoard[row][col] = player;
+    gameBoard[row][col] = player;
   }
 
-  let winner; 
+  let winner;
 
-  for(const combinations of WINNING_COMBINATIONS)
-  {
+  for (const combinations of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combinations[0].row][combinations[0].column];
     const secondSquareSymbol = gameBoard[combinations[1].row][combinations[1].column];
     const thirdSquareSymbol = gameBoard[combinations[2].row][combinations[2].column];
 
     // cheking if all three squares are occupied by the same player
-    if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol)
-    {
-      winner = firstSquareSymbol;
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -81,15 +80,33 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChanged(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        //javascript's computed property name syntax
+        [symbol]: newName
+      };
+    });
+  }
+
   return (
     <>
       <main>
         <div id='game-container'>
           <ol id='players' className='highlight-player'>
-            <Player initialName={PLAYERS.X} symbol='X' isActive={activePlayer === 'X'} />
-            <Player initialName={PLAYERS.O} symbol='O' isActive={activePlayer === 'O'} />
+            <Player initialName={PLAYERS.X}
+              symbol='X'
+              isActive={activePlayer === 'X'}
+              onChangeName={handlePlayerNameChanged}
+            />
+            <Player initialName={PLAYERS.O}
+              symbol='O'
+              isActive={activePlayer === 'O'}
+              onChangeName={handlePlayerNameChanged}
+            />
           </ol>
-          {(winner || hasDrawn) && <GameOver winner={winner} onRestart={handleRestart}/>}
+          {(winner || hasDrawn) && <GameOver winner={winner} onRestart={handleRestart} />}
           <GameBoard
             onSelectSquare={handleSelectSquare}
             board={gameBoard} />
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -3,7 +3,7 @@ export default function GameOver({winner, onRestart})
     return (
         <div id="game-over">
             <h2>Game Over</h2>
-            {winner && <p>Congratulations! {winner} You won!</p>}
+            {winner && <p>Congratulations! {winner} won!</p>}
             {!winner && <p>It is a drawn</p>}
             <p>
             <button onClick={onRestart}>Play Again</button>
